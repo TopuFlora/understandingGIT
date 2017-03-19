a22ea9a3359650ca4102b832f2c6783cd09ec239
akash <shariaretanvir_akash@yahoo.com>
2017-03-20 00:21:21 +0600

    1234-4

--- Azure/Program.cs
+++ Azure/Program.cs
@@ -13,7 +13,6 @@ namespace Azure
         public string[] ColumnNames { get; set; }
         public string[,] Values { get; set; }
         public int LL { get; set; }
-        public int LLL { get; set; }
         public int Abar { get; set; }
     }
 
